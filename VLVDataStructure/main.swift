b3f5c30ea609d80cd44837362c6d965a08e5cf78
SamLau888 <samlau888@vip.163.com>
2020-11-28 15:16:18 +0800

    Open Test All

--- VLVDataStructure/main.swift
+++ VLVDataStructure/main.swift
@@ -5,10 +5,10 @@
 //  Created by sam on 2020/11/24.
 //
 
-//LinkedListTest.test()
-//StackTest.test()
-//QueueTest.test()
-//BinaryTreeTest.test()
-//TrieTest.test()
-//HeepTest.test()
+LinkedListTest.test()
+StackTest.test()
+QueueTest.test()
+BinaryTreeTest.test()
+TrieTest.test()
+HeepTest.test()
 PriorityQueueTest.test()
